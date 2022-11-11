774ab30aa7ae85e277f8a2ead68b3bfc39f2a9ad
Sylvain Gadrat <sgadrat@wontfix.it>
2022-11-11 21:22:04 +0100

    interpret pseudo opcodes

--- 6502cli.cpp
+++ 6502cli.cpp
@@ -30,48 +30,75 @@ void show_cpu(mos6502 const &emu) {
 	;
 }
 
+static bool handle_pseudo_opcode(std::string const& line, mos6502& emu) {
+	if (line == "%cpu") {
+		show_cpu(emu);
+	}else {
+		std::cerr << "unknown pseudo opcode " << line << '\n';
+	}
+
+	return true;
+}
+
+static bool handle_assembly(std::string const& line, mos6502& emu) {
+	// Assemble the line
+	{
+		std::ofstream ofs("/tmp/6502cli.tmp.asm");
+		ofs << line << '\n';
+	}
+	int ret = ::system("xa /tmp/6502cli.tmp.asm -o /tmp/6502cli.tmp.compiled");
+	if (WIFSIGNALED(ret) && (WTERMSIG(ret) == SIGINT || WTERMSIG(ret) == SIGQUIT)) {
+		return false;
+	}
+
+	size_t const max_compiled_size = 100;
+	std::array<char, max_compiled_size> compiled = {char(0xea)}; // 0xea == NOP
+
+	std::ifstream compiled_reader("/tmp/6502cli.tmp.compiled");
+	compiled_reader.read(compiled.data(), max_compiled_size);
+
+	// Run emulator on assembled code
+	size_t const compiled_code_offset = memory.size() - compiled.size();
+	std::memcpy(memory.data() + compiled_code_offset, compiled.data(), compiled.size());
+
+	uint32_t max_cycles = 1'000'000;
+	uint64_t cycles_count = 0;
+	emu.pc = compiled_code_offset;
+	emu.Run(max_cycles, cycles_count, mos6502::CYCLE_COUNT);
+
+	return true;
+}
+
 int main() {
 	// Init emulator
 	memory.fill(0);
 	mos6502 emu(&read_memory, &write_memory);
-	uint64_t cycles_count = 0;
 
 	// Run lines
 	while (true) {
 		// Fetch line from stdin
-		char* line = readline(">>> ");
-		if (!line) {
+		char* cline = readline(">>> ");
+		if (!cline) {
 			std::cout << '\n';
 			break;
 		}
-		add_history(line);
+		add_history(cline);
+		std::string line(cline);
+		free(cline);
 
-		// Assemble the line
-		{
-			std::ofstream ofs("/tmp/6502cli.tmp.asm");
-			ofs << line << '\n';
-			free(line);
-		}
-		int ret = ::system("xa /tmp/6502cli.tmp.asm -o /tmp/6502cli.tmp.compiled");
-		if (WIFSIGNALED(ret) && (WTERMSIG(ret) == SIGINT || WTERMSIG(ret) == SIGQUIT)) {
+		// Select action
+		bool ok = true;
+		switch (line[0]) {
+			case '%':
+				ok = handle_pseudo_opcode(line, emu);
+				break;
+			default:
+				ok = handle_assembly(line, emu);
+				break;
+		};
+
+		if (!ok) {
 			break;
 		}
-
-		size_t const max_compiled_size = 100;
-		std::array<char, max_compiled_size> compiled = {char(0xea)}; // 0xea == NOP
-
-		std::ifstream compiled_reader("/tmp/6502cli.tmp.compiled");
-		compiled_reader.read(compiled.data(), max_compiled_size);
-
-		// Run emulator on assembled code
-		size_t const compiled_code_offset = memory.size() - max_compiled_size;
-		std::memcpy(memory.data() + compiled_code_offset, compiled.data(), max_compiled_size);
-
-		uint32_t max_cycles = 1'000'000;
-		emu.pc = compiled_code_offset;
-		emu.Run(max_cycles, cycles_count, mos6502::CYCLE_COUNT);
-
-		// Show CPU state
-		show_cpu(emu);
 	}
 }
